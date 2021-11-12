227702cd116087cf790b79f53eef087f20b41dc6
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-11-12 14:17:07 -0800

    removing contact form from menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -17,9 +17,6 @@ export default function Menu({ menuOpen, setMenuOpen }) {
         <li onClick={()=>setMenuOpen(false)}>
           <a href="#testimonials">Testimonials</a>
         </li>
-        <li onClick={()=>setMenuOpen(false)}>
-          <a href="#contact">Contact</a>
-        </li>
       </ul>
     </div>
   );

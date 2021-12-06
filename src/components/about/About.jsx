d7e532c9d8d891e2f1df2903a96d32e5f74136b6
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-12-06 14:59:22 -0800

    styling about me section and adding pics

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,94 +14,48 @@ import "./about.scss";
 export default function About() {
 
   return (
-    <div className="about" id="about">
-        <h1>About Me</h1>
-      <div className="wrapper">
-        <h3 className="user" id="font">
-          Helping customer reach their needs is my passion. I am a Project Manager and Full Stack Web Developer committed to deliver successful business solution through timely, measurable, and role-driven projects in fast paced environment.
-          I create successful responsive websites that are fast, easy to use, and built with best practices. The main area of my expertise is front-end development, HTML, CSS, JS, building small and medium web apps, custom plugins, features, animations, and coding interactive layouts.
-        </h3>
-      </div>
-      <br />
-      <h2>Programming Languages</h2>
-      <div className="wrapper">
-        <article>
-          <img className="user" src={CSS} title="CSS" alt="" />
-          <img className="user" src={Html} title="HTML" alt="" />
-          <img className="user" src={JS} title="JavaScript" alt="" />
-          <img className="user" src={Mongodb} title="MongoDb" alt="" />
-          <img className="user" src={MUI} title="Material UI" alt="" />
-          <img className="user" src={Node} title="NodeJs" alt="" />
-          <img className="user" src={NPM} title="NPM" alt="" />
-          <img className="user" src={ReactI} title="React" alt="" />
-          <img className="user" src={SASS} title="SASS" alt="" />
-          <img className="user" src={SQL} title="MySql" alt="" />
-        </article>
-      </div>
-      <br />
-      <br />
-      <h2 className="right"><span>Recent Work Experinece</span></h2>
-      <div className="user">
-        <article>
-          <ul>
-            <li>
-              <h3 >Project Manager/Training Supervisor, November 2016 – Present
-                VIP Hospitality, LLC, SeaTac Airport, WA, USA</h3>
-            </li>
-            <br />
-            <li>
-              <h4>
-                Key Accomplishments:
-              </h4>
-            </li>
-            <br />
-            <li>
-              <h4>
-                Trained 200+ junior and senior employees on development and implementation of training and operation programs.
-              </h4>
-            </li>
-            <br />
-            <li>
-              <h4>
-                Developed and implemented 10+ training projects and 5+ operation projects.
-              </h4>
-            </li>
-            <br />
-            <li>
-              <h4>
-                Conducted training risk assessment on 3+ programs and prepared related operation and training strategies.
-              </h4>
-            </li>
-            <br />
-            <li>
-              <h4>
-                Scheduled projects and ensured its compliance with airport SLA programs development that led to winning WORLD AIRPORT SKYTRAX AWARDS 2019 – BEST AIRPORT STAFF IN NORTH AMERICA 2019 (International Arrivals).
-              </h4>
-            </li>
-            <br />
-            <li>
-              <h4>
-                Assist management in evaluation of training and operation programs and measure their effectiveness.
-              </h4>
-              <br />
-            </li>
-            <li>
-              <h4>
-                Uphold strict compliance with airport procedures and provide timely reports to Port of Seattle and Customs and Border Protection officers for the safety of passengers and employees.
-              </h4>
-            </li>
-            <br />
-            <li>
-              <h4>
-                Monitored all complex projects and implemented development plans, training programs, and strategies to win over new contracts with Port of Seattle and renew exciting ones.
-              </h4>
-            </li>
-          </ul>
-        </article>
+    <main>
+      <section className="about" id="about">
+        <h1 className="title">About Me</h1>
+        <p>Helping customer reach their needs is my passion. I am a Project Manager and Full Stack Web Developer committed to deliver successful business solution through timely, measurable, and role-driven projects in fast paced environment.
+          I create successful responsive websites that are fast, easy to use, and built with best practices. The main area of my expertise is front-end development, HTML, CSS, JS, building small and medium web apps, custom plugins, features, animations, and coding interactive layouts.</p>
+        <h2>Programming Languages</h2>
+        <div className="image-grid">
+          <img src={CSS} title="CSS" alt="" />
+          <img src={Html} title="HTML" alt="" />
+          <img src={JS} title="JavaScript" alt="" />
+          <img src={Mongodb} title="MongoDb" alt="" />
+          <img src={Node} title="NodeJs" alt="" />
+          <img src={MUI} title="Material UI" alt="" />
+          <img src={NPM} title="NPM" alt="" />
+          <img src={ReactI} title="React" alt="" />
+          <img src={SASS} title="SASS" alt="" />
+          <img src={SQL} title="MySql" alt="" />
+        </div>
+       
+        <div className="jobs">
+          <div className="jobs__job">
+            <h2 className="text-secondary">2017 - Current</h2>
+            <h3>Google</h3>
+            <h6>Full Stack Developer</h6>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat vero dolores eum ut, culpa ullam quis quasi exercitationem in adipisci.</p>
+          </div>
+          <div className="jobs__job">
+            <h2 className="text-secondary">2015 - 2017</h2>
+            <h3>Microsoft</h3>
+            <h6>Front End Developer</h6>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat vero dolores eum ut, culpa ullam quis quasi exercitationem in adipisci.</p>
+          </div>
+          <div className="jobs__job">
+            <h2 className="text-secondary">2012 - 2015</h2>
+            <h3>Web Design Co.</h3>
+            <h6>Graphic Designer</h6>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat vero dolores eum ut, culpa ullam quis quasi exercitationem in adipisci.</p>
+          </div>
+        </div>
 
-      </div>
-
-    </div>
+      </section>
+    </main>
   );
 }
 

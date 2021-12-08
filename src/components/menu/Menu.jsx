47105d637b52b9c8d92ef8fa7e9a09f6182d2de6
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-12-08 08:52:38 -0800

    adding clicking outside the menu effect to hide it

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useRef } from "react"
+
 // TODO: import necessary files from sass stylesheets
 import "./menu.scss";
 // TODO: Set up menu optins and onclick events
 export default function Menu({ menuOpen, setMenuOpen }) {
+  const ref = useRef()
+  // Use Effect to control clicking in/out side menu
+  useEffect(() => {
+      // const [menuOpen, setMenuOpen] = useState(false)
+
+    const checkIfClickedOutside = e => {
+      // If the menu is open and the clicked target is not within the menu,
+      // then close the menu
+      if (menuOpen && ref.current && !ref.current.contains(e.target)) {
+        setMenuOpen(false);
+      }
+    };
+    // Add event listeners to click outside the menu
+    document.addEventListener("click", checkIfClickedOutside);
+
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("click", checkIfClickedOutside)
+    }
+  })
   return (
-    <div className={"menu "+(menuOpen && "active")}>
+    <div className={"menu " + (menuOpen && "active")} ref={ref}>
       <ul>
         <li onClick={()=>setMenuOpen(false)}>
           <a href="#intro">Home</a>
@@ -11,10 +33,6 @@ export default function Menu({ menuOpen, setMenuOpen }) {
         <li onClick={()=>setMenuOpen(false)}>
           <a href="#portfolio">Portfolio</a>
         </li>
-        {/* for future projects this section will be very useful */}
-        {/* <li onClick={()=>setMenuOpen(false)}>
-          <a href="#works">Works</a>
-        </li> */}
         <li onClick={()=>setMenuOpen(false)}>
           <a href="#testimonials">Testimonials</a>
         </li>
